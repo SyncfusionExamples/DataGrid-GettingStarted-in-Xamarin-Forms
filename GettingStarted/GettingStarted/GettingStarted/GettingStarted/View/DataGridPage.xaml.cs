bfda7a1b88fd9c94f12c822c48daa1d8258b8b97
FarjanaParveen <farjanaparveena@syncfusion.com>
2019-11-12 15:02:46 +0530

    GettingStarted UG sample modified and committed

--- GettingStarted/GettingStarted/GettingStarted/GettingStarted/View/DataGridPage.xaml.cs
+++ GettingStarted/GettingStarted/GettingStarted/GettingStarted/View/DataGridPage.xaml.cs
@@ -15,48 +15,6 @@ namespace GettingStarted
         public DataGridPage()
         {
             InitializeComponent();
-
-            //GridGroupSummaryRow summaryRow = new GridGroupSummaryRow();
-            //summaryRow.Title = "Total Salary:{TotalSalary} for {ProductCount} members";
-            //summaryRow.ShowSummaryInRow = true;
-            //summaryRow.SummaryColumns.Add(new GridSummaryColumn()
-            //{
-            //    Name = "TotalSalary",
-            //    MappingName = "Salary",
-            //    Format = "{Sum:c}",
-            //    SummaryType = SummaryType.DoubleAggregate
-            //});
-            //summaryRow.SummaryColumns.Add(new GridSummaryColumn()
-            //{
-            //    Name = "ProductCount",
-            //    MappingName = "Salary",
-            //    Format = "{Count}",
-            //    SummaryType = SummaryType.CountAggregate
-            //});
-            //sfGrid.CaptionSummaryRow = summaryRow;
-            //Columns are manually generated instead of auto generating based on model properties.
-            //sfGrid.AutoGenerateColumns = false;
-
-            //GridTextColumn orderIdColumn = new GridTextColumn();
-            //orderIdColumn.MappingName = "OrderID";
-            //orderIdColumn.HeaderText = "Order ID";
-
-            //GridTextColumn customerIdColumn = new GridTextColumn();
-            //customerIdColumn.MappingName = "CustomerID";
-            //customerIdColumn.HeaderText = "Customer ID";
-
-            //GridTextColumn customerColumn = new GridTextColumn();
-            //customerColumn.MappingName = "Customer";
-            //customerColumn.HeaderText = "Customer";
-
-            //GridTextColumn countryColumn = new GridTextColumn();
-            //countryColumn.MappingName = "ShipCountry";
-            //countryColumn.HeaderText = "Country";
-
-            //sfGrid.Columns.Add(orderIdColumn);
-            //sfGrid.Columns.Add(customerIdColumn);
-            //sfGrid.Columns.Add(customerColumn);
-            //sfGrid.Columns.Add(countryColumn);
         }
     }
 }
